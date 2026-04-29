6591a950c29e16b06bf21fabc42604a86126b945
Dhanaraj <dhanarajramesh77@gmail.com>
2026-04-29 11:20:45 +0530

    java prs
    Signed-off-by: Dhanaraj <dhanarajramesh77@gmail.com>

---
+++ file1.java
@@ -0,0 +1,23 @@
+
+class file1 {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+        // int a = 10;
+        // if(a == 1){
+        //     System.out.println("True");
+        // }
+        int mark = 85;
+        if(mark >= 90){
+            System.out.println("Grade A");
+        }else if(mark >= 80){
+            System.out.println("Grade B");
+        }else if(mark >= 70){
+            System.out.println("Grade C");
+        }else if(mark >= 60){
+            System.out.println("Grade D");
+        } else {
+            System.out.println("Grade F");
+        }
+    }
+    
+}
